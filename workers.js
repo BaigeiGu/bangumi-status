8d7fb726204b58fee74576776407bea9f9d60a22
白给 <2271007263A@gmail.com>
2023-12-02 22:53:41 +0800

    Hi

---
+++ workers.js
@@ -0,0 +1,73 @@
+const config = {
+    bangumi_id: 39947
+}
+
+async function getKeys() {
+    const keys = await KV.list()
+    return new Response(JSON.stringify(keys));
+}
+
+async function getValues() {
+    const keys = await KV.list()
+    var r = {}
+    for (let index = 0; index < keys.keys.length; index++) {
+        const element = keys.keys[index]['name'];
+        r[element] = JSON.parse(await KV.get(element))
+
+    }
+    return new Response(JSON.stringify(r));
+}
+
+function update() {
+    fetch(`https://api.bilibili.com/pgc/web/season/stat?season_id=${config.bangumi_id}`)
+        .then((response) => {
+            response.json().then((data) => {
+                var re = data['result']
+                var res = {
+                    coin: re['coins'],
+                    danmakus: re['danmakus'],
+                    follow: re['follow'],
+                    likes: re['likes'],
+                    series_follow: re['series_follow'],
+                    shares: re['shares'],
+                    views: re['views'],
+                }
+                KV.put(Date.now(), JSON.stringify(res))
+            })
+        })
+}
+
+
+async function handleRequest(request) {
+    const requestURL = new URL(request.url);
+    const path = requestURL.pathname.split("/");
+    const params = requestURL.search;
+
+    // console.log(path)
+
+    switch (path[1]) {
+        case 'update':
+            update()
+            return fetch(`https://api.bilibili.com/pgc/web/season/stat?season_id=${config.bangumi_id}`)
+            break
+        case 'get':
+            if (path[2] == 'values') {
+                return getValues()
+            }
+            else {
+                return getKeys()
+            }
+            break
+    }
+
+}
+
+addEventListener('scheduled', update)
+addEventListener('fetch', (event) => {
+    event.respondWith(
+        handleRequest(event.request)
+            .catch(
+                (err) => new Response(err.stack, { status: 500 })
+            )
+    );
+});
