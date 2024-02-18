a1711d113afb3e2bdb085517474e547a16fa6511
BaigeiGu <45301993+BaigeiGu@users.noreply.github.com>
2024-02-18 09:51:01 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,76 +1,88 @@
 const config = {
-    bangumi_id: 39947
+  bangumi_id: 39947
 }
 
 async function getKeys(cursor = undefined) {
-    const keys = await KV.list({ cursor: cursor })
-    return new Response(JSON.stringify(keys));
+  const keys = await KV.list({ cursor: cursor })
+  return new Response(JSON.stringify(keys));
 }
 
 async function getValues(cursor = undefined) {
-    const keys = await KV.list({ cursor: cursor })
-    var r = {}
-    for (let index = 0; index < keys.keys.length; index++) {
-        const element = keys.keys[index]['name'];
-        r[element] = JSON.parse(await KV.get(element))
+  const keys = await KV.list({ cursor: cursor })
+  var r = {}
+  for (let index = 0; index < keys.keys.length; index++) {
+    const element = keys.keys[index]['name'];
+    r[element] = JSON.parse(await KV.get(element))
 
-    }
-    r['cursor'] = keys.cursor
-    return new Response(JSON.stringify(r));
+  }
+  r['cursor'] = keys.cursor
+  return new Response(JSON.stringify(r));
 }
 
-function update() {
-    fetch(`https://api.bilibili.com/pgc/web/season/stat?season_id=${config.bangumi_id}`)
-        .then((response) => {
-            response.json().then((data) => {
-                var re = data['result']
-                var res = {
-                    coin: re['coins'],
-                    danmakus: re['danmakus'],
-                    follow: re['follow'],
-                    likes: re['likes'],
-                    series_follow: re['series_follow'],
-                    shares: re['shares'],
-                    views: re['views'],
-                }
-                KV.put(Date.now(), JSON.stringify(res))
-            })
-        })
+async function read_website_content(url) {
+  console.log("reading website content");
+
+  const response = await fetch(url);
+  const body = await response.json();
+  let cheerioBody = await cheerio.load(body);
+  const resp = {
+    website_body: cheerioBody("p").text(),
+    url: url
+  }
+  return JSON.stringify(resp);
 }
 
+async function update() {
+  const response = await fetch(`https://api.bilibili.com/pgc/web/season/stat?season_id=${config.bangumi_id}`)
+  const r = await response.json()
+
+  var result = r['result']
+  
+  var res = {
+    coin: result['coins'],
+    danmakus: result['danmakus'],
+    follow: result['follow'],
+    likes: result['likes'],
+    series_follow: result['series_follow'],
+    shares: result['shares'],
+    views: result['views'],
+    timestamp : Date.now()
+  }
+  console.log(res)
+  await KV.put(Date.now(), JSON.stringify(res))
+  
+  return new Response(JSON.stringify(res))
+}
 
 async function handleRequest(request) {
-    const requestURL = new URL(request.url);
-    const path = requestURL.pathname.split("/");
-    const params = requestURL.search;
+  const requestURL = new URL(request.url);
+  const path = requestURL.pathname.split("/");
+  const params = requestURL.search;
 
-    // console.log(path)
-
-    switch (path[1]) {
-        case 'update':
-            update()
-            return fetch(`https://api.bilibili.com/pgc/web/season/stat?season_id=${config.bangumi_id}`)
-            break
-        case 'get':
-            if (path[2] == 'values') {
-                return getValues(params['cursor'])
-            }
-            else {
-                return getKeys(params['cursor'])
-            }
-            break
-        default:
-            return getKeys()
-    }
+  switch (path[1]) {
+    case 'update':
+      return await update()
+      break
+    case 'get':
+      if (path[2] == 'values') {
+        return getValues(params['cursor'])
+      }
+      else {
+        return getKeys(params['cursor'])
+      }
+      break
+    default:
+      return getKeys()
+  }
 
 }
 
 addEventListener('scheduled', update)
 addEventListener('fetch', (event) => {
-    event.respondWith(
-        handleRequest(event.request)
-            .catch(
-                (err) => new Response(err.stack, { status: 500 })
-            )
-    );
+  event.respondWith(
+    handleRequest(event.request)
+      .catch(
+        (err) => new Response(err.stack, { status: 500 })
+      )
+  );
 });
